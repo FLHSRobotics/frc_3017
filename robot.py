1ccc3c380a90dfc42871b88fd37929bdcca932e3
YoGurt31 <gurtejsingh0301@gmail.com>
2024-03-08 15:08:58 -0500

    Attempt 1 at Swerve Drive code

--- robot.py
+++ robot.py
@@ -1,67 +1,52 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
-from enum import IntEnum
 import logging
+import math
 
 import rev
 import wpilib
 import wpilib.drive
+import ctre
 
-class Intake(IntEnum):
-    OFF = 0
-    EAT = 1
-    SPIT = 2
+
 
 @dataclass
-class ProcessedInputs:
-    """ dataclass to capture controller inputs """
-    vel_pos: float = 0.0
-    vel_angle: float = 0.0
-    intake_state: int = Intake.OFF
+class SwerveModule:
+    def __init__(self, drive_motor_id, steer_motor_id, encoder_id, offset):
+        self.drive_motor = ctre.WPI_TalonFX(drive_motor_id)
+        self.steer_motor = ctre.WPI_TalonFX(steer_motor_id)
+        self.encoder = ctre.CANCoder(encoder_id)
+        self.offset = offset
+        self.encoder.configMagnetOffset(self.offset)
 
-class SimpleArcadeBot(wpilib.TimedRobot):
+    def set_speed_and_angle(self, speed, angle):
+        current_angle = self.encoder.getAbsolutePosition()
+        target_angle = math.degrees(angle) % 360
+        angle_difference = target_angle - current_angle
+        angle_difference = (angle_difference + 180) % 360 - 180
+        if abs(angle_difference) > 90:
+            angle_difference = (angle_difference + 180) % 360 - 180
+            speed = -speed
+        self.steer_motor.set(ctre.ControlMode.Position, math.radians(current_angle + angle_difference))
+        self.drive_motor.set(ctre.ControlMode.PercentOutput, speed)
+
+
+
+
+class SwerveDriveBot(wpilib.TimedRobot):
     def robotInit(self):
-        """
-        This function is called upon program startup and
-        should be used for any initialization code.
-        """
         # Setting Params
         self.timer = wpilib.Timer()
-        self.logger = logging.getLogger('SimpleArcadeBot')
+        self.logger = logging.getLogger('SwerveDriveBot')
 
         # NO MAGIC VALUES!
-        joystick_usb_port = 0
-        left_lead_motor_channel = 5
-        left_follow_motor_channel = 1
-        right_lead_motor_channel = 2
-        right_follow_motor_channel = 3
-        intake_motor_channel = 4
-        
-        is_left_lead_motor_reversed = False
-        is_left_follow_motor_reversed = False
-        is_right_lead_motor_reversed = True
-        is_right_follow_motor_reversed = True
-        is_intake_motor_reversed = False
-
-        # Initializing Hardware
-        self.left_lead_motor = rev.CANSparkMax( left_lead_motor_channel, rev.CANSparkMax.MotorType.kBrushless )
-        self.left_follow_motor = rev.CANSparkMax( left_follow_motor_channel, rev.CANSparkMax.MotorType.kBrushless )
-        self.right_lead_motor = rev.CANSparkMax( right_lead_motor_channel, rev.CANSparkMax.MotorType.kBrushless )
-        self.right_follow_motor = rev.CANSparkMax( right_follow_motor_channel, rev.CANSparkMax.MotorType.kBrushless )
-        self.intake_motor = rev.CANSparkMax( intake_motor_channel, rev.CANSparkMax.MotorType.kBrushless )
-        self.joystick = wpilib.Joystick( joystick_usb_port )
-
-        # Configuration
-        self.left_lead_motor.setInverted( is_left_lead_motor_reversed )
-        self.left_follow_motor.setInverted( is_left_follow_motor_reversed )
-        self.right_lead_motor.setInverted( is_right_lead_motor_reversed )
-        self.right_follow_motor.setInverted( is_right_follow_motor_reversed )
-        self.left_follow_motor.follow( self.left_lead_motor )
-        self.right_follow_motor.follow( self.right_lead_motor )
-        self.intake_motor.setInverted( is_intake_motor_reversed )
-
-        self.drivetrain = wpilib.drive.DifferentialDrive( self.left_lead_motor, self.right_lead_motor )
-
+        self.joystick = wpilib.Joystick(0)
+        self.swerve_modules = [
+            SwerveModule(1, 2, 1, 0),
+            SwerveModule(3, 4, 2, 0),
+            SwerveModule(5, 6, 3, 0),
+            SwerveModule(7, 8, 4, 0),
+        ]
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -75,52 +60,23 @@ class SimpleArcadeBot(wpilib.TimedRobot):
         """This function is called once each time the robot enters teleoperated mode."""
         pass
 
-    def process_player_input(self):
-        """ Gets inputs from joystick and converts to set of goals """
-        # Joystick Config
-        invert_roll = True
-        invert_pitch = False
-        spit_button = 2
-
+    def teleopPeriodic(self):
+        """This function is called periodically during teleoperated mode."""
         # Process
-        data = ProcessedInputs()
-        data.vel_pos = (-1)**invert_roll * self.joystick.getY()
-        data.vel_angle = (-1)**invert_pitch * self.joystick.getX()
+        Drive = -self.joystick.getY()
+        Strafe = self.joystick.getX()
+        Rotate = self.joystick.getZ()
 
-        if self.joystick.getTrigger():
-            data.intake_state = Intake.EAT
-        elif self.joystick.getRawButton(spit_button):
-            data.intake_state = Intake.SPIT
+        for module in self.swerve_modules:
+            module.set_speed_and_angle(math.hypot(Drive, Strafe), math.atan2(Drive, Strafe))
 
         # Telemetry
         self.logger.info("=========================")
         self.logger.info("Player inputs:")
-        self.logger.info(f"\tGoal Velocity: {data.vel_pos}")
-        self.logger.info(f"\tGoal Angular Velocity: {data.vel_angle}")
-        self.logger.info(f"\tIntake: {data.intake_state}")
-        return data
-
-    def actuate(self, goal):
-        # angle needs to be negated
-        # https://robotpy.readthedocs.io/projects/robotpy/en/stable/wpilib.drive/DifferentialDrive.html#wpilib.drive.DifferentialDrive
-        self.drivetrain.arcadeDrive( goal.vel_pos, -goal.vel_angle )
-
-        match goal.intake_state:
-            case Intake.EAT:
-                self.intake_motor.set(1.0)
-
-            case Intake.SPIT:
-                self.intake_motor.set(-1.0)
-
-            case Intake.OFF:
-                self.intake_motor.set(0)
-
-
-    def teleopPeriodic(self):
-        """This function is called periodically during teleoperated mode."""
-        goals = self.process_player_input()
-        self.actuate(goals)
-
+        self.logger.info(f"\tGoal Velocity (Y): {Drive}")
+        self.logger.info(f"\tGoal Velocity (X): {Strafe}")
+        self.logger.info(f"\tGoal Angular Velocity: {Rotate}")
+        return Drive, Strafe, Rotate
 
     def testInit(self):
         """This function is called once each time the robot enters test mode."""
@@ -130,5 +86,6 @@ class SimpleArcadeBot(wpilib.TimedRobot):
         """This function is called periodically during test mode."""
         pass
 
+
 if __name__ == "__main__":
-    wpilib.run(SimpleArcadeBot)
+    wpilib.run(SwerveDriveBot)
